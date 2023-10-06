3101f9e2f81ccdeb9054f103aa53ad805bacd367
Paarth314 <paarthiyer1234@gmail.com>
2023-10-06 18:26:45 +0530

    fix: tried to fix everything

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-    import '$src/app.css'
+    import '../app.css'
 </script>
 
 <div>
-    <h1 class="text-lg bg-accent p-5 rounded-md text-accent-content">Welcome to SvelteKit</h1>
+    <h1 class="rounded-md bg-accent p-5 text-lg text-accent-content">Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </div>
