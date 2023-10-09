e7990f02998996156aa04130fa24da2cdc69e745
Paarth314 <paarthiyer1234@gmail.com>
2023-10-10 00:12:09 +0530

    feat: changed navbar and added some text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,12 @@
 <div class="flex w-full">
     <div class="mx-auto h-full w-full max-w-screen-md px-4">
         <div class="w-full border-t-[1px] border-accent py-6">
-            <h1 class="my-10 text-start text-5xl font-bold">Hello visitor</h1>
-            <p class="my-2">I will add more stuff soon</p>
+            <h1 class="mb-6 mt-10 text-start text-3xl font-bold sm:text-5xl">Hello visitor</h1>
+            <p class="my-2 text-lg">
+                I am Paarth Iyer, a 21-year-old currently enrolled in the MSc program for Computer
+                Science at the Chennai Mathematical Institute.
+            </p>
+            <!-- <p class="my-2">I will add more stuff soon</p> -->
         </div>
     </div>
 </div>
