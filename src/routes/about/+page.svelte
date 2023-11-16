a9d4310182a81ae0855b092784a8efeadadb8936
Paarth314 <paarthiyer1234@gmail.com>
2023-11-16 11:56:59 +0530

    refactor: cleanup

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,22 +1,22 @@
 <script>
     import ComingSoon from '$src/lib/components/ComingSoon.svelte'
-    import Header from '$src/lib/components/MarkdownStyles/Header.svelte'
-    import SvelteMarkdown from 'svelte-markdown'
-    const source = `
-# This is a header
-  
-  This is a paragraph.
-  
-  * This is a list
-  * With two items
-    1. And a sublist
-    2. That is ordered
-      * With another
-      * Sublist inside
-  
-  | And this is | A table |
-  |-------------|---------|
-  | With two    | columns |`
+    // import Header from '$src/lib/components/MarkdownStyles/Header.svelte'
+    // import SvelteMarkdown from 'svelte-markdown'
+    //     const source = `
+    // # This is a header
+
+    //   This is a paragraph.
+
+    //   * This is a list
+    //   * With two items
+    //     1. And a sublist
+    //     2. That is ordered
+    //       * With another
+    //       * Sublist inside
+
+    //   | And this is | A table |
+    //   |-------------|---------|
+    //   | With two    | columns |`
 </script>
 
 <!-- <div class=" flex w-full">
