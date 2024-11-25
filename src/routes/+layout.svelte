ace1ff77341951dd2dfc163de0d74893fc436815
Paarth Iyer <78010820+PaarthIyer@users.noreply.github.com>
2024-11-25 22:52:58 +0530

    feat: create the whole thing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,10 @@
     let { children } = $props()
 </script>
 
-{@render children()}
+<div
+    class="flex min-h-screen w-full bg-gray-50 text-zinc-800 selection:bg-accent-400 selection:text-white"
+>
+    <div class="mx-auto w-full max-w-screen-lg bg-white shadow-md shadow-gray-300">
+        {@render children()}
+    </div>
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,43 @@ export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
 
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: {
+                    '50': '#ecf3ff',
+                    '100': '#dce8ff',
+                    '200': '#c0d3ff',
+                    '300': '#9bb5ff',
+                    '400': '#738bff',
+                    '500': '#5264ff',
+                    '600': '#3338f8',
+                    '700': '#292adc',
+                    '800': '#2225b1',
+                    '900': '#24288b',
+                    '950': '#151651'
+                },
+                accent: {
+                    '50': '#effaf3',
+                    '100': '#d9f2e2',
+                    '200': '#b6e4c9',
+                    '300': '#6ec697',
+                    '400': '#53b484',
+                    '500': '#319868',
+                    '600': '#217a52',
+                    '700': '#1b6143',
+                    '800': '#174e36',
+                    '900': '#14402e',
+                    '950': '#0a241a'
+                }
+            },
+            fontFamily: {
+                altheading: ['Libre Baskerville', 'serif'],
+                heading: ['Montserrat', 'sans-serif'],
+                body: ['Montserrat', 'sans-serif'],
+                // body: ['Source Sans 3', 'sans-serif'],
+                altbody: ['Hind', 'sans-serif']
+            }
+        }
     },
 
     plugins: [typography, containerQueries]
